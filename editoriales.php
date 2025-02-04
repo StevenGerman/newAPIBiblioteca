29e65999bd86917abdeb2a2cd92081441f27a21d
StevenGerman <germansteven2024@gmail.com>
2025-02-04 10:29:48 -0300

    edi

--- editoriales.php
+++ editoriales.php
@@ -39,53 +39,16 @@ switch ($_SERVER['REQUEST_METHOD']) {
 
 // Maneja solicitudes GET
 function handleGetRequest($pdo) {
-    /*
-    GET
-Por ID (idAutor):
-Parámetro: idEditorial=1
-Retorno: Información del editorial con ID 1.
-Por nombre (ediNombre):
-Parámetro: ediNombre=Planeta
-Retorno: Información de los editoriales cuyo nombre contenga "Planeta".
+        $sql = "SELECT * FROM editoriales";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        header("HTTP/1.1 200 OK");
+        echo json_encode($stmt->fetchAll());
     
-    */
-    // Si se proporciona el parámetro 'idEditorial', busca por ID
-    if (isset($_GET['idEditorial'])) {
-        $sql = $pdo->prepare("SELECT * FROM Editoriales WHERE idEditorial=:idEditorial");
-        $sql->bindValue(':idEditorial', $_GET['idEditorial']);
-        $sql->execute();
-        $sql->setFetchMode(PDO::FETCH_ASSOC);
-        header("HTTP/1.1 200 OK");
-        echo json_encode($sql->fetchAll());
-    } 
-    
-    // Si se proporciona el autámetro 'ediNombre', busca por nombre
-    elseif (isset($_GET['ediNombre'])) {
-        $ediNombre = strtolower($_GET['ediNombre']);
-        $sql = $pdo->prepare("SELECT * Editoriales WHERE LOWER(ediNombre) LIKE :ediNombre");
-        $sql->bindValue(':ediNombre', '%' . $ediNombre . '%', PDO::PARAM_STR);
-        $sql->execute();
-        $sql->setFetchMode(PDO::FETCH_ASSOC);
-        header("HTTP/1.1 200 OK");
-        echo json_encode($sql->fetchAll());
-    } 
-    // Si no se proporciona ningún parámetro, obtiene todos las editoriales 
-    else {
-        $sql = $pdo->prepare("SELECT * FROM Editoriales");
-        $sql->execute();
-        $sql->setFetchMode(PDO::FETCH_ASSOC);
-        header("HTTP/1.1 200 OK");
-        echo json_encode($sql->fetchAll());
-    }
     exit;
 }
 
-function handlePostRequest($pdo){
-}
-function handlePutRequest($pdo){
-}
-function handleDeleteRequest($pdo){
-}
 
 
 
