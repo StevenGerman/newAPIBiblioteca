2f09325b3d66f0fb5d8920707f256902724373c8
StevenGerman <germansteven2024@gmail.com>
2025-02-04 12:34:03 -0300

    roles

---
+++ roles.php
@@ -0,0 +1,41 @@
+<?php
+
+// Habilita el acceso desde cualquier origen
+header("Access-Control-Allow-Origin: *");
+// Habilita los métodos HTTP permitidos
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+// Establece el tipo de contenido de la respuesta como JSON
+header("Content-Type: application/json; charset=UTF-8");
+// Habilita las cabeceras permitidas
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+include 'conexion.php';
+
+$pdo = new Conexion();
+
+switch($_SERVER['REQUEST_METHOD']){
+    case 'GET':
+        getRequest($pdo);
+        break;
+    case 'POST':
+        postRequest($pdo);
+        break;
+    case 'PUT':
+        putRequest($pdo);
+        break;
+    case 'DELETE':
+        deleteRequest($pdo);
+        break;
+
+}
+
+function getRequest($pdo){
+    $sql = "SELECT * FROM roles";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    header("HTTP/1.1 200 OK");
+    echo json_encode($stmt->fetchAll());
+}
+
+?>
