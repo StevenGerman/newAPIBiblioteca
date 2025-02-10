caaf5fc26af92bae89cdc5a119ef6332e5dd21cc
StevenGerman <germansteven2024@gmail.com>
2025-02-10 08:29:25 -0300

    get-libro

--- libros.php
+++ libros.php
@@ -1,10 +1,48 @@
 <?php
 
+// Habilita el acceso desde cualquier origen
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Method: GET,POST,PUT,DELETE,OPTIONS");
-header("Content-Type: application/json; charset= UTF-8");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With")
+// Habilita los métodos HTTP permitidos
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+// Establece el tipo de contenido de la respuesta como JSON
+header("Content-Type: application/json; charset=UTF-8");
+// Habilita las cabeceras permitidas
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
+include 'conexion.php';
+
+$pdo = new Conexion();
+
+switch ($_SERVER['REQUEST_METHOD']) {
+    case 'GET':
+        getRequest($pdo);
+        break;
+    case 'POST':
+        postRequest($pdo);
+        break;
+    case 'PUT':
+        putRequest($pdo);
+        break;
+    case 'DELETE':
+        deleteRequest($pdo);
+        break;
+    case 'OPTIONS':
+        // Maneja las solicitudes preflight
+        header("HTTP/1.1 200 OK");
+        break;
+    default:
+        header("HTTP/1.1 405 Method Not Allowed");
+        break;
+}
+
+function getRequest($pdo){
+    $sql = $pdo->prepare("SELECT * FROM libros");
+        $sql->execute();
+        $sql->setFetchMode(PDO::FETCH_ASSOC);
+        header("HTTP/1.1 200 OK");
+        echo json_encode($sql->fetchAll());
+    exit;
+}
 
 
 
